33e4693ae97a300451d74c5f7ebf3a481bf7df38
NoelMartinson <nmartinson@student.neumont.edu>
2025-07-01 10:17:43 -0600

    Week 1, Day 2

---
+++ Source/Engine/Core/Math.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace fox {
+	constexpr float pi = 3.1415926535f;
+	constexpr float twoPi = 2 * pi;
+	constexpr float haflPi = pi / 2;
+}
--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -1,5 +1,9 @@
+#include "Core/Math.h"
 #include <iostream>
-int main()
-{
+
+#define NAME "Agone\n"
+int main(){
+	std::cout << NAME;
 	std::cout << "Hello, World!\n";
+	std::cout << fox::pi << std::endl;
 }
